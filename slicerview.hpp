76fea4986d5ad59025350ab2ee62ea95557609df
Julián Unrrein <junrrein@gmail.com>
2017-10-09 19:39:34 -0300

    slicerview.hpp: Remove commented-out code

--- slicerview.hpp
+++ slicerview.hpp
@@ -13,15 +13,6 @@ public:
     virtual ~View(){};
 
 private:
-    // A class to store a child with its associated page index
-    //    class ViewChild : public Gtk::FlowBoxChild {
-    //    public:
-    //        ViewChild(Gdk::Pixbuf pixbuf, int pageIndex);
-
-    //    private:
-    //        const int m_pageIndex;
-    //    };
-
     void onChildActivated(Gtk::FlowBoxChild* child);
 
     Slicer::Document& m_document;
