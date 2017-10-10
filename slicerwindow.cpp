83887f86d86a54a8ed98b5c67a906d2ec6eeb718
Julián Unrrein <junrrein@gmail.com>
2017-10-10 02:22:23 -0300

    Slicer::Window: Add Headerbar with Save button. Make the button save the document to an user-defined place. Show a notification when we are done saving

--- slicerwindow.cpp
+++ slicerwindow.cpp
@@ -1,17 +1,88 @@
 #include "slicerwindow.hpp"
+#include <gtkmm/filechooserdialog.h>
+#include <glibmm/main.h>
 
 namespace Slicer {
 
 Window::Window(std::string filePath)
     : m_document{filePath}
     , m_view{m_document}
+    , m_labelDone{"Saved!"}
 {
-    set_title("PDF Slicer");
+    // Widget setup
+    set_titlebar(m_headerBar);
     set_default_size(800, 600);
 
+    m_headerBar.set_title("PDF Slicer");
+    m_headerBar.set_show_close_button(true);
+    m_headerBar.set_has_subtitle(false);
+
+    m_buttonSave.set_image_from_icon_name("document-save");
+    m_headerBar.pack_start(m_buttonSave);
+
     m_scroller.add(m_view);
-    add(m_scroller);
+
+    m_labelDone.set_margin_left(10);
+    m_labelDone.set_margin_right(10);
+    m_labelDone.set_margin_top(10);
+    m_labelDone.set_margin_bottom(10);
+    m_buttonDoneClose.set_image_from_icon_name("window-close",
+                                               Gtk::ICON_SIZE_SMALL_TOOLBAR);
+    m_boxDone.pack_start(m_labelDone);
+    m_boxDone.pack_start(m_buttonDoneClose);
+    auto styleContext = m_boxDone.get_style_context();
+    styleContext->add_class("osd");
+    m_revealerDone.add(m_boxDone);
+    m_revealerDone.set_halign(Gtk::ALIGN_CENTER);
+    m_revealerDone.set_valign(Gtk::ALIGN_START);
+
+    m_overlay.add(m_scroller);
+    m_overlay.add_overlay(m_revealerDone);
+
+    add(m_overlay);
+
+    // Signal handlers
+    m_buttonSave.signal_clicked().connect([this]() {
+        onSaveAction();
+    });
+
+    m_buttonDoneClose.signal_clicked().connect([this]() {
+        m_revealerDone.set_reveal_child(false);
+    });
+
+    m_signalSaved.connect([this]() {
+        // Stop any previous hiding animation started
+        if (m_connectionSaved.connected())
+            m_connectionSaved.disconnect();
+
+        // Show notification and automatically hide it later
+        m_revealerDone.set_reveal_child(true);
+        m_connectionSaved = Glib::signal_timeout().connect([this]() {
+            m_revealerDone.set_reveal_child(false);
+            return false;
+        },
+                                                           5000);
+    });
 
     show_all_children();
+}
+
+void Window::onSaveAction()
+{
+    Gtk::FileChooserDialog dialog{*this,
+                                  "Save file as",
+                                  Gtk::FILE_CHOOSER_ACTION_SAVE};
+    dialog.set_transient_for(*this);
+
+    dialog.add_button("Cancel", Gtk::RESPONSE_CANCEL);
+    dialog.add_button("Save", Gtk::RESPONSE_OK);
+
+    const int result = dialog.run();
+
+    if (result == Gtk::RESPONSE_OK) {
+        std::string filePath = dialog.get_filename();
+        m_document.saveDocument(filePath);
+        m_signalSaved.emit();
+    }
 }
 }
--- slicerwindow.hpp
+++ slicerwindow.hpp
@@ -2,8 +2,13 @@
 #define SLICERWINDOW_HPP
 
 #include "slicerview.hpp"
-#include <gtkmm/window.h>
 #include <gtkmm/scrolledwindow.h>
+#include <gtkmm/headerbar.h>
+#include <gtkmm/button.h>
+#include <gtkmm/overlay.h>
+#include <gtkmm/revealer.h>
+#include <gtkmm/label.h>
+#include <gtkmm/box.h>
 
 namespace Slicer {
 
@@ -13,9 +18,29 @@ public:
     virtual ~Window(){};
 
 private:
+    // Model
     Slicer::Document m_document;
+
+    // Header bar
+    Gtk::HeaderBar m_headerBar;
+    Gtk::Button m_buttonSave;
+
+    // View
     Slicer::View m_view;
     Gtk::ScrolledWindow m_scroller;
+    Gtk::Overlay m_overlay;
+
+    Gtk::Revealer m_revealerDone;
+    Gtk::Box m_boxDone;
+    Gtk::Label m_labelDone;
+    Gtk::Button m_buttonDoneClose;
+
+    // Signals
+    sigc::signal<void> m_signalSaved;
+    sigc::connection m_connectionSaved;
+
+    // Callbacks
+    void onSaveAction();
 };
 }
 
