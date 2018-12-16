b2283263236ccd77de29c5e7013ced0f00479e20
Julián Unrrein <junrrein@gmail.com>
2018-12-16 16:23:30 -0300

    Remove all non-site-related files

--- cmake-modules/Utils.cmake
+++
@@ -1,8 +0,0 @@
-function (target_link_libraries_system target)
-	set (libs ${ARGN})
-	foreach (lib ${libs})
-		get_target_property (lib_include_dirs ${lib} INTERFACE_INCLUDE_DIRECTORIES)
-		target_include_directories (${target} SYSTEM PRIVATE ${lib_include_dirs})
-		target_link_libraries (${target} ${lib})
-	endforeach (lib)
-endfunction (target_link_libraries_system)
