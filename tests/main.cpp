3bf05df9b44a24107da5997a2c957282cc0e8fda
Julián Unrrein <junrrein@gmail.com>
2019-01-09 19:06:57 -0300

    Tests: Add more initialization

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,13 @@
 #define CATCH_CONFIG_RUNNER
 
 #include <catch.hpp>
+#include <config.hpp>
 #include <gtkmm/main.h>
 
 int main(int argc, char* argv[])
 {
     Gtk::Main::init_gtkmm_internals();
+    Slicer::config::createSlicerDirsIfNotExistent();
 
     int result = Catch::Session().run(argc, argv);
 
