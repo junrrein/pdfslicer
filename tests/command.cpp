841e4e3a7e08a45b55727dd5c990b659ca9c3209
Julián Unrrein <junrrein@gmail.com>
2019-05-03 23:58:14 -0300

    Tests/Command: Add tests for AddFileCommand

--- tests/command.cpp
+++ tests/command.cpp
@@ -7,8 +7,11 @@
 using namespace Slicer;
 
 static const std::string multipage1Name = "multipage-1";
+static const std::string multipage2Name = "multipage-2";
 static const std::string multipage1Path
     = Glib::build_filename(Glib::get_current_dir(), multipage1Name + ".pdf");
+static const std::string multipage2Path
+    = Glib::build_filename(Glib::get_current_dir(), multipage2Name + ".pdf");
 
 bool areIndexesMonoIncreasing(const Document& doc);
 
@@ -610,6 +613,165 @@ SCENARIO("Moving 2 adjacent pages across different places of a document, using t
 
                 THEN("Indexes should start from 0 and be monotonically increasing")
                 REQUIRE(areIndexesMonoIncreasing(doc));
+            }
+        }
+    }
+}
+
+SCENARIO("Adding a new file to an existing document, using the Command abstraction")
+{
+    GIVEN("A multipage PDF document with 15 pages")
+    {
+        auto multipagePdfFile = Gio::File::create_for_path(multipage1Path);
+        Document doc{multipagePdfFile};
+        REQUIRE(doc.numberOfPages() == 15);
+
+        WHEN("A 5-page PDF file is added to the beggining")
+        {
+            AddFileCommand command{doc, Gio::File::create_for_path(multipage2Path), 0};
+            command.execute();
+
+            THEN("The document should now have 20 pages")
+            REQUIRE(doc.numberOfPages() == 20);
+
+            THEN("The first page of the document should be the first page of the second file")
+            {
+                REQUIRE(doc.getPage(0)->fileIndex() == 0);
+                REQUIRE(doc.getPage(0)->fileName() == multipage2Name);
+            }
+
+            THEN("The 5th page of the document should be the last page of the second file")
+            {
+                REQUIRE(doc.getPage(4)->fileIndex() == 4);
+                REQUIRE(doc.getPage(4)->fileName() == multipage2Name);
+            }
+
+            THEN("The 6th page of the document should be the first page of the first file")
+            {
+                REQUIRE(doc.getPage(5)->fileIndex() == 0);
+                REQUIRE(doc.getPage(5)->fileName() == multipage1Name);
+            }
+
+            THEN("The last page of the document should be the last page of the first file")
+            {
+                REQUIRE(doc.getPage(19)->fileIndex() == 14);
+                REQUIRE(doc.getPage(19)->fileName() == multipage1Name);
+            }
+
+            WHEN("The command is undone")
+            {
+                command.undo();
+
+                THEN("The document should now have 15 pages")
+                REQUIRE(doc.numberOfPages() == 15);
+
+                THEN("The 1st page of the document should be the 1st page of the first file")
+                {
+                    REQUIRE(doc.getPage(0)->fileIndex() == 0);
+                    REQUIRE(doc.getPage(0)->fileName() == multipage1Name);
+                }
+            }
+        }
+
+        WHEN("A 5-page PDF file is added to the end")
+        {
+            AddFileCommand command{doc, Gio::File::create_for_path(multipage2Path), doc.numberOfPages()};
+            command.execute();
+
+            THEN("The document should now have 20 pages")
+            REQUIRE(doc.numberOfPages() == 20);
+
+            THEN("The first page of the document should be the first page of the first file")
+            {
+                REQUIRE(doc.getPage(0)->fileIndex() == 0);
+                REQUIRE(doc.getPage(0)->fileName() == multipage1Name);
+            }
+
+            THEN("The 15th page of the document should be the last page of the first file")
+            {
+                REQUIRE(doc.getPage(14)->fileIndex() == 14);
+                REQUIRE(doc.getPage(14)->fileName() == multipage1Name);
+            }
+
+            THEN("The 16th page of the document should be the first page of the second file")
+            {
+                REQUIRE(doc.getPage(15)->fileIndex() == 0);
+                REQUIRE(doc.getPage(15)->fileName() == multipage2Name);
+            }
+
+            THEN("The last page of the document should be the last page of the second file")
+            {
+                REQUIRE(doc.getPage(19)->fileIndex() == 4);
+                REQUIRE(doc.getPage(19)->fileName() == multipage2Name);
+            }
+
+            WHEN("The commmand is undone")
+            {
+                command.undo();
+
+                THEN("The document should now have 15 pages")
+                REQUIRE(doc.numberOfPages() == 15);
+
+                THEN("The last page of the document should be the last page of the first file")
+                {
+                    REQUIRE(doc.getPage(14)->fileIndex() == 14);
+                    REQUIRE(doc.getPage(14)->fileName() == multipage1Name);
+                }
+            }
+        }
+
+        WHEN("A 5-page PDF file is added at the 5th page of the document")
+        {
+            AddFileCommand command{doc, Gio::File::create_for_path(multipage2Path), 4};
+            command.execute();
+
+            THEN("The document should now have 20 pages")
+            REQUIRE(doc.numberOfPages() == 20);
+
+            THEN("The 4th page of the document should be the 4th page of the first file")
+            {
+                REQUIRE(doc.getPage(3)->fileIndex() == 3);
+                REQUIRE(doc.getPage(3)->fileName() == multipage1Name);
+            }
+
+            THEN("The 5th page of the document should be the 1st page of the second file")
+            {
+                REQUIRE(doc.getPage(4)->fileIndex() == 0);
+                REQUIRE(doc.getPage(4)->fileName() == multipage2Name);
+            }
+
+            THEN("The 9th page of the document should be the 5th page of the second file")
+            {
+                REQUIRE(doc.getPage(8)->fileIndex() == 4);
+                REQUIRE(doc.getPage(8)->fileName() == multipage2Name);
+            }
+
+            THEN("The 10th page of the document should be the 5th page of the first file")
+            {
+                REQUIRE(doc.getPage(9)->fileIndex() == 4);
+                REQUIRE(doc.getPage(9)->fileName() == multipage1Name);
+            }
+
+            WHEN("The command is undone")
+            {
+                command.undo();
+
+                THEN("The document should now have 15 pages")
+                REQUIRE(doc.numberOfPages() == 15);
+
+                THEN("The 5th and 6th pages of the document should be the 5th and 6th pages of the first file")
+                {
+                    REQUIRE(doc.getPage(4)->fileIndex() == 4);
+                    REQUIRE(doc.getPage(4)->fileName() == multipage1Name);
+                    REQUIRE(doc.getPage(5)->fileIndex() == 5);
+                    REQUIRE(doc.getPage(5)->fileName() == multipage1Name);
+                }
+
+                THEN("The last page of the document should be the last page of the first file")
+                {
+                    REQUIRE(doc.getPage(14)->fileIndex() == 14);
+                    REQUIRE(doc.getPage(14)->fileName() == multipage1Name);
+                }
             }
         }
     }
