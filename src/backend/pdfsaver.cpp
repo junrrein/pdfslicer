3461337a30bf34918a19c098889ff4964a66fdd0
Julián Unrrein <junrrein@gmail.com>
2020-03-21 00:44:41 -0300

    PdfSaver: Improve file size of documents that are smaller than the original

--- src/backend/pdfsaver.cpp
+++ src/backend/pdfsaver.cpp
@@ -1,6 +1,8 @@
 #include "pdfsaver.hpp"
 #include "tempfile.hpp"
 #include <qpdf/QPDFWriter.hh>
+#include <range/v3/view/iota.hpp>
+#include <range/v3/view/set_algorithm.hpp>
 
 namespace Slicer {
 
@@ -32,16 +34,37 @@ void PdfSaver::persist(const Glib::RefPtr<Gio::File>& destinationFile)
     // This preserves the metadata and outline of that file.
     QPDF* destinationPDF = m_filesData.front().qpdf.get();
     QPDFPageDocumentHelper* destinationPageDocumentHelper = m_filesData.front().qpdfPageDocumentHelper.get();
+    const std::vector<QPDFPageObjectHelper> originalPages = destinationPageDocumentHelper->getAllPages();
 
-    for (auto qpdfPage : m_filesData.front().qpdfPages)
+    for (auto qpdfPage : originalPages)
         destinationPageDocumentHelper->removePage(qpdfPage);
+
+    // Add the wanted pages into the document.
+    // It's necessary to keep track of which pages from the original document
+    // got inserted.
+    std::set<int> preserverdPagesFromOriginalFile;
 
     for (PageData page : m_saveData.pages) {
         QPDFPageObjectHelper qpdfPage = m_filesData.at(page.file).qpdfPages.at(page.pageNumber);
         qpdfPage.rotatePage(page.rotation, false);
         destinationPageDocumentHelper->addPage(qpdfPage, false);
+
+        if (page.file == 0)
+            preserverdPagesFromOriginalFile.insert(static_cast<int>(page.file));
     }
 
+    // It's necessary to manually delete the page objects of the pages
+    // from the original PDF which were deleted.
+    // Taken from https://github.com/qpdf/qpdf/blob/release-qpdf-9.1.1/qpdf/qpdf.cc#L4980
+    auto pagesFromOriginalFile = ranges::views::ints(0, static_cast<int>(originalPages.size()));
+    auto removedPagesFromOriginalFile = ranges::views::set_difference(pagesFromOriginalFile, preserverdPagesFromOriginalFile);
+
+    for (int pageNumber : removedPagesFromOriginalFile)
+        destinationPDF->replaceObject(
+            originalPages.at(static_cast<unsigned>(pageNumber)).getObjectHandle().getObjGen(),
+            QPDFObjectHandle::newNull());
+
+    // Write the result to a file
     QPDFWriter writer{*destinationPDF};
     writer.setOutputFilename(destinationFile->get_path().c_str());
     writer.write();
