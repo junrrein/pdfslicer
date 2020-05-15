78c7f7182747e4b2f9fad1d45565c7de87ef359c
miltolstoy <miltolstoy@gmail.com>
2020-05-15 20:09:45 +0300

    copyright update

--- src/application/previewheaderbar.hpp
+++ src/application/previewheaderbar.hpp
@@ -1,5 +1,5 @@
 // PDF Slicer
-// Copyright (C) 2017-2020 Julián Unrrein
+// Copyright (C) 2020 Mil Tolstoy
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
