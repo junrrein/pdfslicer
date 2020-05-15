882abc4b103ae5254d082e8a0b531fabc1d94128
miltolstoy <miltolstoy@gmail.com>
2020-05-15 13:57:33 +0300

    refactor(PreviewWindow): move zoom buttons to window title

--- src/application/previewwindow.hpp
+++ src/application/previewwindow.hpp
@@ -20,6 +20,7 @@
 #include <page.hpp>
 #include "pagewidget.hpp"
 #include "taskrunner.hpp"
+#include "previewheaderbar.hpp"
 #include "zoomlevelwithactions.hpp"
 #include <glibmm/dispatcher.h>
 #include <giomm/simpleactiongroup.h>
@@ -58,9 +59,7 @@ private:
     Gtk::ScrolledWindow m_scroller;
     Gtk::EventBox m_eventBox;
     std::shared_ptr<PageWidget> m_pageWidget;
-	Gtk::Button m_buttonZoomIn;
-	Gtk::Button m_buttonZoomOut;
-	Gtk::Box m_boxZoom;
+    PreviewHeaderBar m_previewHeaderBar;
 
     void setTitle();
 	void setupWidgets();
